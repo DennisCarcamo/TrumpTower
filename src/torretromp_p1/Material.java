4e944b04b57769508ef84fd97590dfa7e31a9dc1
Ibrahim Herrera <alexping7@gmail.com>
2016-05-11 15:43:03 -0600

    Material normal

--- src/torretromp_p1/Material.java
+++ src/torretromp_p1/Material.java
@@ -2,6 +2,46 @@
 package torretromp_p1;
 
 public class Material {
-    String nombre, descripcion, marca;
-    int numeroDeSerie;
+    String nombre,descripcion, marca;
+     int numeroDeSerie;
+   
+    public Material() {
+    }
+
+    public Material(String nombre, String descripcion, String marca, int numeroDeSerie) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.marca = marca;
+        this.numeroDeSerie = numeroDeSerie;
+        
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getdescripcion() {
+        return descripcion;
+    }
+
+    public void setdescripcion(String direccion) {
+        this.descripcion = direccion;
+    }
+
+    public int getnumeroDeSerie() {
+        return numeroDeSerie;
+    }
+
+    public void setnumeroDeSerieId(int id) {
+        this.numeroDeSerie = numeroDeSerie;
+    }
+
+ 
+
+   
+     
 }
